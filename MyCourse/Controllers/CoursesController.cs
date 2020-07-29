c53d33e6a1d2e25eca7e543811aada912dd3152a
Oddi Fabrizio <oddifabrizio@hotmail.it>
2020-07-29 12:36:24 +0200

    Aggiunte view di contenuto e una view di layout

--- MyCourse/Controllers/CoursesController.cs
+++ MyCourse/Controllers/CoursesController.cs
@@ -6,13 +6,13 @@ namespace MyCourse.Controllers
     {
        public IActionResult Index()
        {
-           return Content("Sono Index");
+           return View();
        } 
 
 
        public IActionResult Detail(string id)
        {
-           return Content($"Sono Detail, ho ricevuto l'id {id}");
+           return View();
        }
 
     }
